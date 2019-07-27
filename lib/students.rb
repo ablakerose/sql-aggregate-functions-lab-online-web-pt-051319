2eb978577742dcf477c2fa68c4badea2a288220f
Blake Rose <bdecktor@gmail.com>
2019-07-27 20:09:26 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,7 +9,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
+  "SELECT AVG(gpa) FROM students WHERE grade == 9;"
 end
 
 def total_tardies_for_all_students
